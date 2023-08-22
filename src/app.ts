f3ae8209fe9b9e60d7fbc6deb1bb3fcf8c2cd592
Brian Etaghene <joveee05@gmail.com>
2023-08-22 02:37:58 +0100

    Added more classes and custom types

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
+enum ProjectStatus {
+  active,
+  finished,
+}
+
+class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) {}
+}
+
 class ProjectState {
   private listeners: any[] = [];
   private projects: any[] = [];
@@ -18,12 +33,14 @@ class ProjectState {
   }
 
   addProjects(title: string, description: string, numOfPeople: number) {
-    const newProject = {
-      id: Math.random().toString(),
-      title: title,
-      description: description,
-      people: numOfPeople,
-    };
+    const newProject = new Project(
+      Math.random().toString(),
+      title,
+      description,
+      numOfPeople,
+      ProjectStatus.active
+    );
+
     this.projects.push(newProject);
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice());
@@ -79,7 +96,7 @@ class ProjectList {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLElement;
-  assignedProjects: any[];
+  assignedProjects: Project[];
 
   constructor(private type: 'active' | 'finished') {
     this.templateElement = document.getElementById(
